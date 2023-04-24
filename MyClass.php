3ba96b2758fec294ca66c666916ea91595587276
fmihel <fmihel76@gmail.com>
2023-04-24 09:57:32 +0300

    stash

--- MyClass.php
+++ MyClass.php
@@ -2,14 +2,36 @@
 
 namespace MyVendor;
 
+use Composer\Installer\PackageEvent;
 use Composer\Script\Event;
 
 class MyClass
 {
+    public static function postUpdate(Event $event)
+    {
+        $composer = $event->getComposer();
+        error_log('postUpdate...');
+        // do stuff
+    }
+
+    public static function postAutoloadDump(Event $event)
+    {
+        $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
+        require $vendorDir . '/autoload.php';
+        error_log('postAutoloadDump...');
+
+    }
+
+    public static function postPackageInstall(PackageEvent $event)
+    {
+        $installedPackage = $event->getOperation()->getPackage();
+        // do stuff
+        error_log('postPackageInstall...');
+    }
 
     public static function warmCache(Event $event)
     {
         // make cache toasty
-        error_log('message from Vendor!!!!');
+        error_log('warmCache...');
     }
 }
